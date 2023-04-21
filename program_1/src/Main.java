f107201ef83c213890ed58c72886ec1be6ab9c2d
Vlad devoloper <vladmed118@gmail.com>
2023-04-21 22:44:36 +0300

    program

---
+++ program_1/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    public static String calc(String input) throws Exception {
+        String[] input_split = input.split(" ");
+        if (input_split.length != 3) {
+            throw new Exception("Length Exceptions");
+        }
+        int a = Integer.parseInt(input_split[0]);
+        int b = Integer.parseInt(input_split[2]);
+        if(a>10||b>10||a<1||b<1)
+        {
+            throw new Exception("Format Exceptions");
+        }
+        int result = 0;
+        switch (input_split[1]) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default: throw new Exception("Sign Exception");
+
+        }
+        return Integer.toString(result);
+
+        //throw new Exception("throws Exception");
+    }
+    public static void main(String[] args) throws Exception{
+
+            Scanner in = new Scanner(System.in);
+        while (true) {
+            String str = in.nextLine();
+            if (str=="") {
+                break;
+            }
+            try {
+                String out = calc(str);
+                System.out.println(out);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                break;
+            }
+        }
+    }
+}
+
